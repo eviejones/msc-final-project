b488fd2aa23956cb79ddbd388610214eafb7dfea
Evie Jones <147057981+eviejones@users.noreply.github.com>
2026-07-30 15:36:38 +0100

    feat:add processing files

---
+++ processing/acled_processing.py
@@ -0,0 +1,203 @@
+import pandas as pd
+import numpy as np
+from ingest.acled_client import AcledClient
+from utils.dates import *
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def mark_conflict_events(df: pd.DataFrame) -> pd.DataFrame:
+    """Takes the ACLED dataframe and marks event as conflict (1) or not conflict (0).
+
+     Conflict events are used to create the target Y.
+
+     Args:
+         df (pd.DataFrame): The full ACLED dataframe
+    Returns:
+        pd.DataFrame: The dataframe with am additional column 'conflict' with binary markers.
+    """
+
+    acled_subevent_mapping = {
+        # BATTLES (Conflict)
+        "Armed clash": 1,
+        "Government regains territory": 1,
+        "Non-state actor overtakes territory": 1,
+        # EXPLOSIONS / REMOTE VIOLENCE (Conflict)
+        "Air/drone strike": 1,
+        "Chemical weapon": 1,
+        "Remote explosive/landmine/IED": 1,
+        "Shelling/artillery/missile attack": 1,
+        "Suicide bomb": 1,
+        "Grenade": 1,
+        # VIOLENCE AGAINST CIVILIANS (Conflict)
+        "Abduction/forced disappearance": 1,
+        "Attack": 1,
+        "Sexual violence": 1,
+        # RIOTS (Conflict)
+        "Mob violence": 1,
+        "Violent demonstration": 1,
+        # PROTESTS (Non-conflict)
+        "Excessive force against protesters": 0,
+        "Peaceful protest": 0,
+        "Protest with intervention": 0,
+        # STRATEGIC DEVELOPMENTS (Non-conflict)
+        "Agreement": 0,
+        "Arrests": 0,
+        "Change to group/activity": 0,
+        "Disrupted weapons use": 0,
+        "Headquarters or base established": 0,
+        "Looting/property destruction": 0,
+        "Non-violent transfer of territory": 0,
+        "Other": 0,
+    }
+    df["conflict"] = df["sub_event_type"].apply(lambda x: acled_subevent_mapping[x])
+    return df  # TODO add validation
+
+def create_regional_monthly_baseline(df: pd.DataFrame, k: float) -> pd.DataFrame:
+    """Outputs dataframe with regional monthly conflict events and marked escalations.
+
+    1. Groups data by region and month, counting conflict events
+    2. Expands data to include all regions and months
+    3. Creates six monthly rolling average and standard deviation
+    4. Adds a mew column that indicates whether there was an escalation k deviations about the mean.
+
+    Args:
+        df (pd.DataFrame): Full ACLED dataframe including six months build up
+        k (float): The number of standard deviations above the mean for it to be considered an escalation in conflict.
+
+    Returns:
+        pd.DataFrame: Grouped regional dataframe, with conflict escalation marked (Y)
+    """
+    df = df.copy()
+    df_grouped = (
+        df.groupby(["admin1", "year_month"])["conflict"]
+        .sum()
+        .reset_index(name="conflict_event_count")
+    )
+
+    # Build full dataset of all regions and months
+    all_regions = df["admin1"].unique()
+    all_months = pd.period_range(
+        df["year_month"].min(), df["year_month"].max(), freq="M"
+    )
+    full_index = pd.MultiIndex.from_product(
+        [all_regions, all_months], names=["admin1", "year_month"]
+    )
+
+    df_grouped = (
+        df_grouped.set_index(["admin1", "year_month"])
+        .reindex(full_index, fill_value=0)
+        .reset_index()
+        .sort_values(["admin1", "year_month"])
+    )
+
+    df_grouped = df_grouped.sort_values(by=["admin1", "year_month"])
+
+    # Calculate rolling statistics ending at the previous month (t-1)
+    df_grouped["rolling_mean_6m"] = df_grouped.groupby("admin1")[
+        "conflict_event_count"
+    ].transform(lambda x: x.rolling(window=6, min_periods=6).mean().shift(1))
+
+    df_grouped["rolling_std_6m"] = df_grouped.groupby("admin1")[
+        "conflict_event_count"
+    ].transform(lambda x: x.rolling(window=6, min_periods=6).std().shift(1))
+
+    df_grouped["escalation_threshold"] = df_grouped["rolling_mean_6m"] + (
+        k * df_grouped["rolling_std_6m"]
+    )
+
+    # Define the binary target variable (is current conflict > historical threshold?)
+    df_grouped["target_escalation"] = np.where(
+        df_grouped["conflict_event_count"] > df_grouped["escalation_threshold"], 1, 0
+    )
+    logger.info(f"Escalation target set at {k} standard deviations above the mean.")
+
+    return df_grouped
+
+def pre_process_data(
+    df: pd.DataFrame, k: float, event_col: str = "sub_event_type"
+) -> tuple[pd.DataFrame, list[str]]:
+    """Processes data so it is suitable to feed into the model.
+
+    1. Groups data by region and year_month, marks conflict events
+    2. Combines previously grouped data with fatalities and baseline data
+    3. Creates list of columns used for prediction.
+
+    Args:
+        df (pd.DataFrames): Full data from ACLED.
+        k (float): The number of standard deviations above the mean for it to be considered an escalation in conflict.
+        event_col (str): The event column to group on, either sub_event_type or event_type.
+    Returns:
+        pd.DataFrame: The data ready for passing to the model.
+        list[str]: List of columns used for prediction.
+    """
+    if event_col not in ["event_type", "sub_event_type"]:
+        raise ValueError(
+            "Event column must be either 'sub_event_type' or 'event_type'."
+        )
+    df = df.copy()
+    df["year_month"] = pd.to_datetime(df["year_month"]).dt.to_period("M")
+    df = mark_conflict_events(df)
+
+    pivot_df = pd.pivot_table(
+        df,
+        values="event_id_cnty",
+        index=["admin1", "year_month"],
+        columns=[event_col],
+        aggfunc="count",
+        fill_value=0,
+    ).reset_index()
+
+    logger.info(f"Data grouped by {event_col}")
+
+    pivot_df.columns = (
+        pivot_df.columns.str.lower()
+        .str.replace(" ", "_", regex=False)
+        .str.replace("/", "_", regex=False)
+        .str.replace("-", "_", regex=False)
+    )
+
+    baseline_df = create_regional_monthly_baseline(df, k)
+
+    fatalities_df = (
+        df.groupby(["admin1", "year_month"])["fatalities"].sum().reset_index()
+    )
+
+    combined_df = baseline_df.merge(
+        pivot_df, on=["admin1", "year_month"], how="left"
+    ).merge(fatalities_df, on=["admin1", "year_month"], how="left")
+
+    # Define what type of column each is
+    event_cols = pivot_df.columns.drop(["admin1", "year_month"]).tolist()
+    current_event_cols = event_cols + ["fatalities"]
+    lagged_event_cols = ["rolling_mean_6m", "rolling_std_6m", "escalation_threshold"]
+    predictor_cols = current_event_cols + lagged_event_cols
+
+    combined_df[current_event_cols] = (
+        combined_df[current_event_cols]
+        .fillna(0)
+        .groupby(combined_df["admin1"])[current_event_cols]
+        .shift(1)
+    )
+
+    combined_df[predictor_cols] = combined_df[predictor_cols].fillna(0)
+
+    combined_df = (
+        combined_df.rename(columns={"admin1": "region"})
+        .sort_values(by=["year_month", "region"])
+        .reset_index(drop=True)
+    )
+
+    return combined_df, predictor_cols
+
+def get_clean_data(params, download=True):
+    if download:
+        acled = AcledClient()
+        all_data = acled.get_data(countries, start_date, end_date)
+    else:
+        all_data = pd.read_csv("../data/all_data.csv")
+    processed_df, predictor_cols = pre_process_data(
+        all_data, params["k"], params["event_col"]
+    )
+    return processed_df, predictor_cols
---
+++ processing/food_prices_processing.py
@@ -0,0 +1,105 @@
+import pandas as pd
+from ingest.hdx_client import HdxClient
+from utils.dates import *
+from utils.name_mapping import *
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def read_food_prices():
+    hdx = HdxClient()
+    df_sudan = hdx.get_data(dataset_name="wfp-food-prices-for-sudan", file_name="Sudan - Food Prices", file_type='csv')
+    df_south_sudan = hdx.get_data(dataset_name="wfp-food-prices-for-south-sudan", file_name="South Sudan - Food Prices",
+                file_type='csv')
+
+    df_abyei = df_south_sudan[df_south_sudan["market"] == "Abyei"].copy()
+    df_abyei["admin1"] = "Abyei"
+
+    df_combined = pd.concat([df_sudan, df_abyei], ignore_index=True)
+
+    primary_commodities = ["Sorghum", "Millet", "Wheat flour"]
+
+    df_filtered = df_combined[
+        (df_combined["commodity"].isin(primary_commodities))
+        & (df_combined["pricetype"] == "Retail")
+        & (df_combined["priceflag"].isin(["actual", "aggregate"]))
+    ].copy()
+
+    df_filtered_cols = df_filtered[
+        ["date", "admin1", "admin2", "market", "commodity", "unit", "usdprice"]
+    ].copy()
+
+    def clean_state_names(value):
+        return SUDAN_STATE_MAPPING.get(value, value)
+
+
+    renamed_df = df_filtered_cols.copy()
+    renamed_df["admin1"] = df_filtered_cols["admin1"].apply(clean_state_names)
+    renamed_df["date"] = pd.to_datetime(renamed_df["date"])
+    renamed_df['year_month'] = renamed_df['date'].dt.to_period('M')
+    renamed_df["unit_weight_in_kg"] = (
+            renamed_df['unit']
+            .str.extract(r'([\d.]+)', expand=False)
+            .astype(float)
+            .fillna(1.0)
+        )
+    renamed_df["usdprice"] = pd.to_numeric(renamed_df["usdprice"], errors="coerce")
+    renamed_df['usdprice_per_kg'] = renamed_df['usdprice'] / renamed_df['unit_weight_in_kg']
+    return renamed_df
+
+def process_and_pivot_food_prices(df_prices: pd.DataFrame) -> pd.DataFrame:
+    df = df_prices[
+        (df_prices["year_month"] >= start_date) & (df_prices["year_month"] <= end_date)
+        ].copy()
+
+    df_grouped = (
+        df.groupby(["admin1", "year_month", "commodity"])["usdprice_per_kg"]
+        .median()
+        .reset_index()
+    )
+
+    all_regions = df_grouped["admin1"].unique()
+    all_months = pd.period_range(
+        df_grouped["year_month"].min(), df_grouped["year_month"].max(), freq="M"
+    )
+    all_commodities = df_grouped["commodity"].unique()
+
+    full_index = pd.MultiIndex.from_product(
+        [all_regions, all_months, all_commodities],
+        names=["admin1", "year_month", "commodity"],
+    )
+
+    df_expanded = (
+        df_grouped.set_index(["admin1", "year_month", "commodity"])
+        .reindex(full_index)
+        .reset_index()
+        .sort_values(["admin1", "commodity", "year_month"])
+    )
+
+    df_expanded["usdprice_per_kg"] = df_expanded.groupby(
+        ["admin1", "commodity"]
+    )["usdprice_per_kg"].transform(lambda x: x.ffill().bfill())
+
+    df_pivoted = df_expanded.pivot(
+        index=["admin1", "year_month"],
+        columns="commodity",
+        values="usdprice_per_kg",
+    ).reset_index()
+
+    df_pivoted.columns.name = None
+    df_pivoted = df_pivoted.rename(
+        columns={
+            "Millet": "price_millet",
+            "Sorghum": "price_sorghum",
+            "Wheat flour": "price_wheat_flour",
+        }
+    )
+
+    return df_pivoted
+
+def get_clean_data():
+    food_prices_df = read_food_prices()
+    pivoted_df = process_and_pivot_food_prices(food_prices_df)
+    return pivoted_df
