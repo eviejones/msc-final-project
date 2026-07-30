6b85b3df1c42c3676e968f4249f2ee19b9c32478
Evie Jones <147057981+eviejones@users.noreply.github.com>
2026-07-30 15:36:55 +0100

    feat: add utils

---
+++ utils/dates.py
@@ -0,0 +1,12 @@
+countries = ["Sudan"]
+start_date = "2017-07-01"  # TODO validation for 6 month warm up period
+end_date = "2024-12-31"
+
+train_start_date = "2018-01-01"
+train_end_date = "2022-12-31"
+
+onset_start_date = "2023-01-01"
+onset_end_date = "2023-12-31"
+
+active_start_date = "2024-01-01"
+active_end_date = "2024-12-31"
---
+++ utils/name_mapping.py
@@ -0,0 +1,5 @@
+SUDAN_STATE_MAPPING = {"Al Gezira": "Al Jazirah",
+     "Nile": "River Nile",
+     "Eastern Darfur": "East Darfur",
+    'Abyei PCA': 'Abyei',
+    'Aj Jazirah': 'Al Jazirah'}
