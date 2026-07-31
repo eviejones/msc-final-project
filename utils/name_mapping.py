2c4d43ed89a277ebb40944b1675925438db24c60
Evie Jones <147057981+eviejones@users.noreply.github.com>
2026-07-31 14:11:31 +0100

    feat: add code mapping

--- utils/name_mapping.py
+++ utils/name_mapping.py
@@ -5,3 +5,8 @@ SUDAN_STATE_MAPPING = {
     "Abyei PCA": "Abyei",
     "Aj Jazirah": "Al Jazirah",
 }
+
+SUDAN_PCODE_MAPPING = {'SD19': 'Abyei PCA', 'SD15': 'Aj Jazirah', 'SD08': 'Blue Nile', 'SD06': 'Central Darfur', 'SD05': 'East Darfur', 'SD12': 'Gedaref', 'SD11': 'Kassala', 'SD01': 'Khartoum', 'SD02': 'North Darfur', 'SD13': 'North Kordofan', 'SD17': 'Northern', 'SD10': 'Red Sea', 'SD16': 'River Nile', 'SD14': 'Sennar', 'SD03': 'South Darfur', 'SD07': 'South Kordofan', 'SD04': 'West Darfur', 'SD18': 'West Kordofan', 'SD09': 'White Nile'}
+
+def clean_state_names(value):
+    return SUDAN_STATE_MAPPING.get(value, value)
