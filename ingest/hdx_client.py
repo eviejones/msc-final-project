1f3110518e7940e94adbd126d8595ffc00c1b518
eviejone <147057981+eviejones@users.noreply.github.com>
2026-08-09 15:01:17 +0100

    fix: re add admin boundaries method from old file

--- ingest/hdx_client.py
+++ ingest/hdx_client.py
@@ -25,6 +25,70 @@ class HdxClient:
         name = file_name.lower().replace("-", "")
         name = re.sub(r"\s+", "_", name.strip())
         return f"hdx_{name}"
+    
+    
+    def get_admin_boundaries(self, dataset_name, file_name, file_type, download=True):
+        download_dir = "data/hdx"
+
+        if not download:
+            file_path = next(
+                (
+                    os.path.join(download_dir, f)
+                    for f in os.listdir(download_dir)
+                    if file_name.lower() in f.lower()
+                    and f.endswith(f".{file_type.lower()}")
+                ),
+                None,
+            )
+
+            if file_path:
+                logger.info(f"download=False: Reading local file {file_path}")
+                return gpd.read_file(file_path)
+
+            logger.error(f"Local file matching '{file_name}.{file_type}' not found.")
+            return None
+
+        dataset = self.data.read_from_hdx(dataset_name)
+        if dataset is None:
+            logger.error("Dataset not found.")
+            return None
+
+        resources = dataset.get_resources()
+        target_resource = next(
+            (r for r in resources if file_type.lower() in r["format"].lower()), None
+        )
+
+        if target_resource:
+            os.makedirs(download_dir, exist_ok=True)
+            _, path = target_resource.download(folder=download_dir)
+            logger.info(f"Downloaded file to: {path}")
+
+            if zipfile.is_zipfile(path):
+                logger.info("Extracting zip archive...")
+                with zipfile.ZipFile(path, "r") as zip_ref:
+                    extract_dir = os.path.dirname(path)
+                    zip_ref.extractall(extract_dir)
+
+                    target_file = next(
+                        (
+                            os.path.join(extract_dir, f)
+                            for f in zip_ref.namelist()
+                            if file_name.lower() in f.lower()
+                            and f.endswith(f".{file_type.lower()}")
+                        ),
+                        None,
+                    )
+
+                    if target_file:
+                        path = target_file
+                    else:
+                        logger.error(
+                            f"Could not find '{file_name}' with extension '.{file_type}' inside the zip."
+                        )
+                        return None
+
+            logger.info(f"Successfully loaded '{file_name}' into dataframe")
+            return gpd.read_file(path)
 
     def get_data(self, dataset_name, file_name, file_type):
         download_dir = "data/hdx"
