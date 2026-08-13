8e11d38c2008498eebfd8f9a3b4b237063a9c2ee
eviejones <evie.jones@cabinetoffice.gov.uk>
2026-08-13 17:25:24 +0100

    fix: remove ethiopia in constants

--- utils/constants.py
+++ utils/constants.py
@@ -19,4 +19,21 @@ ONSET_START_DATE = pd.to_datetime("2023-01-01")
 ONSET_END_DATE = pd.to_datetime("2023-12-31")
 
 ACTIVE_START_DATE = pd.to_datetime("2024-01-01")
-ACTIVE_END_DATE = pd.to_datetime("2024-12-31")
+ACTIVE_END_DATE = pd.to_datetime("2025-12-31")
+
+# COUNTRY = "Ethiopia"
+
+# PRIMARY_COMMODITIES = [
+#     "Sorghum",
+#     "Teff",
+#     "Maize",
+# ]  # List of primary food commodities to focus on for analysis, this would be dependent on a country
+
+
+# TRAIN_START_DATE = pd.to_datetime("2015-07-01")
+# TRAIN_END_DATE   = pd.to_datetime("2020-06-30")   
+# ONSET_START_DATE = pd.to_datetime("2020-07-01")
+# ONSET_END_DATE   = pd.to_datetime("2021-06-30")  
+# ACTIVE_START_DATE = pd.to_datetime("2021-07-01")
+# ACTIVE_END_DATE  = pd.to_datetime("2022-06-30")
+
