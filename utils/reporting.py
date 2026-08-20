c1b0541b14ecae57511ed14b60d1cff2a2942808
eviejones <evie.jones@cabinetoffice.gov.uk>
2026-08-20 13:45:11 +0100

    feat: add reporting utils

--- utils/reporting.py
+++ utils/reporting.py
@@ -35,3 +35,33 @@ def open_model_report(label):
     onset_predictions = pd.read_csv(REPORTS_DIR / f"{label_formatted}_onset_predictions.csv")
     
     return results, best_params, shap_importance, onset_predictions
+
+def read_model_reports(files):
+    result_list = []
+    shap_list = []
+    onset_list = []
+    
+    for f in files:
+        result, best_params, shap_importance, onset_predictions = open_model_report(f)
+        
+        model_name = result['model'].iloc[0] 
+        
+        shap_importance['model'] = model_name
+        onset_predictions['model'] = model_name
+        
+        result_list.append(result)
+        shap_list.append(shap_importance)
+        onset_list.append(onset_predictions)
+        
+    all_results = pd.concat(result_list)
+    all_shap = pd.concat(shap_list)
+    all_onset = pd.concat(onset_list)
+    
+    df_melted = all_results.melt(
+        id_vars=['model'], 
+        var_name='metric', 
+        value_name='score'
+    )
+    df_melted['score'] = pd.to_numeric(df_melted['score'], errors='coerce')
+    
+    return df_melted, all_shap, all_onset
