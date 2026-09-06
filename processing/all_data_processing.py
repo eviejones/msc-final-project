725283523c3bab7265c9fcde89281b2f638b04dd
eviejone <147057981+eviejones@users.noreply.github.com>
2026-09-06 11:28:50 +0100

    fix: update logger

--- processing/all_data_processing.py
+++ processing/all_data_processing.py
@@ -12,7 +12,7 @@ from utils.dates import (
 )
 from utils.logger import get_logger
 
-logger = get_logger("Data preparation")
+logger = get_logger("All data processing")
 
 def get_clean_combined_data(
     data_sources: list[str] | None = None,
