23c7d023def363e1bc892d6ddcc13d6104134dea
Evie Jones <147057981+eviejones@users.noreply.github.com>
2026-09-11 09:42:20 +0100

    fix: clean up constants
    Removed commented-out code

--- utils/constants.py
+++ utils/constants.py
@@ -29,19 +29,3 @@ ONSET_END_DATE = pd.to_datetime("2023-12-31")
 ACTIVE_START_DATE = pd.to_datetime("2024-01-01")
 ACTIVE_END_DATE = pd.to_datetime("2025-12-31")
 
-# ---- Ethiopia Example, don't uncomment unless you want to run the project for Ethiopia
-
-# COUNTRY = "Ethiopia"
-
-# PRIMARY_COMMODITIES = [
-#     "Sorghum",
-#     "Teff",
-#     "Maize",
-# ]  # List of primary food commodities to focus on for analysis, this would be dependent on a country
-
-# TRAIN_START_DATE = pd.to_datetime("2015-07-01")
-# TRAIN_END_DATE   = pd.to_datetime("2020-06-30")
-# ONSET_START_DATE = pd.to_datetime("2020-07-01")
-# ONSET_END_DATE   = pd.to_datetime("2021-06-30")
-# ACTIVE_START_DATE = pd.to_datetime("2021-07-01")
-# ACTIVE_END_DATE  = pd.to_datetime("2022-06-30")
